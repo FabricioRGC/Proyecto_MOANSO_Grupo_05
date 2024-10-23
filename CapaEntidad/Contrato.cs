f80cfd01f5567507d0b6d77c9b95e5edd3b7292f
Dario Rodriguez <valentinorodriguezlozada@gmail.com>
2024-10-23 13:51:16 -0500

    Agregando las clases necesarias para agregar la lógica de los contratos

---
+++ CapaEntidad/Contrato.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Contrato
+    {
+        public class entContrato
+        {
+            public string clienteId { get; set; }
+            public string fechaInicio { get; set; }
+            public string duracion { get; set; }
+            public string estado { get; set; }
+            public string tipo_plan { get; set; }
+            public int monto { get; set; }
+        }
+    }
+}
