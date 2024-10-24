0d0b1ce932ce72cbd8062a9c7a22e9f66e85b09b
Jxrsxn <jerson2004dt@gmail.com>
2024-10-24 11:01:07 -0500

    cambio diseño de respuesto

--- Proyecto_MOANSO_Grupo_05/RepuestosForm.Designer.cs
+++ Proyecto_MOANSO_Grupo_05/RepuestosForm.Designer.cs
@@ -44,13 +44,17 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridRepuestos)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridRepuestos
             // 
             this.dataGridRepuestos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridRepuestos.Location = new System.Drawing.Point(43, 223);
+            this.dataGridRepuestos.Location = new System.Drawing.Point(65, 323);
             this.dataGridRepuestos.Name = "dataGridRepuestos";
             this.dataGridRepuestos.RowHeadersWidth = 57;
             this.dataGridRepuestos.RowTemplate.Height = 24;
@@ -60,7 +64,7 @@
             // 
             // txtBuscarR
             // 
-            this.txtBuscarR.Location = new System.Drawing.Point(571, 71);
+            this.txtBuscarR.Location = new System.Drawing.Point(580, 161);
             this.txtBuscarR.Name = "txtBuscarR";
             this.txtBuscarR.Size = new System.Drawing.Size(172, 22);
             this.txtBuscarR.TabIndex = 32;
@@ -69,7 +73,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(468, 74);
+            this.label6.Location = new System.Drawing.Point(477, 164);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(52, 16);
             this.label6.TabIndex = 31;
@@ -77,7 +81,7 @@
             // 
             // btnInhabilitarRepuesto
             // 
-            this.btnInhabilitarRepuesto.Location = new System.Drawing.Point(528, 143);
+            this.btnInhabilitarRepuesto.Location = new System.Drawing.Point(537, 233);
             this.btnInhabilitarRepuesto.Name = "btnInhabilitarRepuesto";
             this.btnInhabilitarRepuesto.Size = new System.Drawing.Size(148, 43);
             this.btnInhabilitarRepuesto.TabIndex = 30;
@@ -87,7 +91,7 @@
             // 
             // btnModificarRepuesto
             // 
-            this.btnModificarRepuesto.Location = new System.Drawing.Point(338, 143);
+            this.btnModificarRepuesto.Location = new System.Drawing.Point(347, 233);
             this.btnModificarRepuesto.Name = "btnModificarRepuesto";
             this.btnModificarRepuesto.Size = new System.Drawing.Size(148, 43);
             this.btnModificarRepuesto.TabIndex = 29;
@@ -97,7 +101,7 @@
             // 
             // btnRegistrarRepuesto
             // 
-            this.btnRegistrarRepuesto.Location = new System.Drawing.Point(136, 143);
+            this.btnRegistrarRepuesto.Location = new System.Drawing.Point(145, 233);
             this.btnRegistrarRepuesto.Name = "btnRegistrarRepuesto";
             this.btnRegistrarRepuesto.Size = new System.Drawing.Size(148, 43);
             this.btnRegistrarRepuesto.TabIndex = 28;
@@ -107,7 +111,7 @@
             // 
             // txtStockR
             // 
-            this.txtStockR.Location = new System.Drawing.Point(571, 24);
+            this.txtStockR.Location = new System.Drawing.Point(580, 114);
             this.txtStockR.Name = "txtStockR";
             this.txtStockR.Size = new System.Drawing.Size(172, 22);
             this.txtStockR.TabIndex = 27;
@@ -121,14 +125,14 @@
             // 
             // txtNombreR
             // 
-            this.txtNombreR.Location = new System.Drawing.Point(233, 74);
+            this.txtNombreR.Location = new System.Drawing.Point(242, 164);
             this.txtNombreR.Name = "txtNombreR";
             this.txtNombreR.Size = new System.Drawing.Size(172, 22);
             this.txtNombreR.TabIndex = 25;
             // 
             // txtCodigoR
             // 
-            this.txtCodigoR.Location = new System.Drawing.Point(233, 24);
+            this.txtCodigoR.Location = new System.Drawing.Point(242, 114);
             this.txtCodigoR.Name = "txtCodigoR";
             this.txtCodigoR.Size = new System.Drawing.Size(172, 22);
             this.txtCodigoR.TabIndex = 24;
@@ -143,7 +147,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(468, 24);
+            this.label5.Location = new System.Drawing.Point(477, 114);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(44, 16);
             this.label5.TabIndex = 22;
@@ -161,7 +165,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(53, 80);
+            this.label3.Location = new System.Drawing.Point(62, 170);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(146, 16);
             this.label3.TabIndex = 20;
@@ -170,7 +174,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(53, 27);
+            this.label2.Location = new System.Drawing.Point(62, 117);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(138, 16);
             this.label2.TabIndex = 19;
@@ -185,11 +189,47 @@
             this.label1.TabIndex = 18;
             this.label1.Text = "ID del Repuesto:";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(580, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 22);
+            this.textBox1.TabIndex = 37;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(242, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 22);
+            this.textBox2.TabIndex = 36;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(464, 68);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 16);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "Descripción:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(77, 62);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 16);
+            this.label8.TabIndex = 34;
+            this.label8.Text = "ID del Repuesto:";
+            // 
             // RepuestosForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 527);
+            this.ClientSize = new System.Drawing.Size(822, 649);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.dataGridRepuestos);
             this.Controls.Add(this.txtBuscarR);
             this.Controls.Add(this.label6);
@@ -232,5 +272,9 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
