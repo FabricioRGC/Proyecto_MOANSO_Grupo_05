578ad38eb4d66cf606171d1ab534c6059eec9050
Walter <Walter@LAPTOP-8O0M843J>
2024-10-25 00:43:53 -0500

    personal

--- Proyecto_MOANSO_Grupo_05/PersonalForm.cs
+++ Proyecto_MOANSO_Grupo_05/PersonalForm.cs
@@ -7,14 +7,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaLogica;
+using CapaEntidad;
+using CapaDatos;
+using static CapaEntidad.Personal;
+using static CapaEntidad.Cliente;
+
 
 namespace Proyecto_MOANSO_Grupo_05
 {
     public partial class PersonalForm : Form
     {
-        public PersonalForm()
-        {
-            InitializeComponent();
+
+        
+            public PersonalForm()
+            {
+                InitializeComponent();
+                listarPersonal();
+            }
+            public void listarPersonal()
+            {
+                tablaPersonal.DataSource = logPersonal.Instancia.listarPersonal();
+            }
+
+            private void LimpiarVariables()
+            {
+                txtNombre.Text = "";
+                txtDisponibilidad.Text = "";
+
+            }
+
+            private void btnAñadir_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    entPersonal cli = new entPersonal();
+                    cli.nombre = txtNombre.Text.Trim();
+                    cli.disponibilidad = txtDisponibilidad.Text.Trim();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                LimpiarVariables();
+                listarPersonal();
+            }
+
+            private void btnInhabilitar_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    entPersonal cli = new entPersonal();
+                    cli.nombre = txtNombre.Text.Trim();
+                    logPersonal.Instancia.DeshabilitarPersonal(cli);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                LimpiarVariables();
+                listarPersonal();
+            }
+
+            private void txtBuscarNombre_TextChanged(object sender, EventArgs e)
+            {
+                string nombre = txtBuscar.Text;
+
+                var clientesfiltrados = logCliente.Instancia.ListarCliente()
+                    .Where(cliente => cliente.nombre.Contains(nombre))
+                    .ToList();
+
+                tablaPersonal.DataSource = clientesfiltrados;
+            }
+
+            private void btnBuscar_Click(object sender, EventArgs e)
+            {
+
+            }
         }
+
+
     }
-}
+
+
