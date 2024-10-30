acf498baf6e4ea56b2be2d1b685936d438ca9231
Steven <st22vsc@gmail.com>
2024-10-30 12:22:56 -0500

    dia 30 cambio pequeño en interfaz

--- Proyecto_MOANSO_Grupo_05/Form1.Designer.cs
+++ Proyecto_MOANSO_Grupo_05/Form1.Designer.cs
@@ -31,11 +31,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.buttonOrdenes = new System.Windows.Forms.Button();
             this.buttonAlmyTra = new System.Windows.Forms.Button();
             this.buttonMantFr = new System.Windows.Forms.Button();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,11 +43,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(98)))), ((int)(((byte)(175)))));
-            this.label1.Font = new System.Drawing.Font("Leelawadee", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(72, 3);
+            this.label1.Location = new System.Drawing.Point(72, 2);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1132, 71);
+            this.label1.Size = new System.Drawing.Size(1189, 69);
             this.label1.TabIndex = 15;
             this.label1.Text = "              Lunnys Perú R2d E.I.R.L.             ";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -60,23 +60,14 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Segoe UI Black", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(3, 85);
+            this.button1.Location = new System.Drawing.Point(3, 140);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(178, 76);
+            this.button1.Size = new System.Drawing.Size(178, 154);
             this.button1.TabIndex = 21;
             this.button1.Text = "Ventas";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.White;
-            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
-            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel2.Location = new System.Drawing.Point(172, 83);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1110, 719);
-            this.panel2.TabIndex = 22;
             // 
             // buttonOrdenes
             // 
@@ -86,9 +77,10 @@
             this.buttonOrdenes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonOrdenes.Font = new System.Drawing.Font("Segoe UI Black", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonOrdenes.ForeColor = System.Drawing.Color.White;
-            this.buttonOrdenes.Location = new System.Drawing.Point(3, 3);
+            this.buttonOrdenes.Location = new System.Drawing.Point(3, 2);
+            this.buttonOrdenes.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonOrdenes.Name = "buttonOrdenes";
-            this.buttonOrdenes.Size = new System.Drawing.Size(178, 76);
+            this.buttonOrdenes.Size = new System.Drawing.Size(178, 134);
             this.buttonOrdenes.TabIndex = 23;
             this.buttonOrdenes.Text = "Ordenes";
             this.buttonOrdenes.UseVisualStyleBackColor = true;
@@ -101,9 +93,10 @@
             this.buttonAlmyTra.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonAlmyTra.Font = new System.Drawing.Font("Segoe UI Black", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonAlmyTra.ForeColor = System.Drawing.Color.White;
-            this.buttonAlmyTra.Location = new System.Drawing.Point(3, 167);
+            this.buttonAlmyTra.Location = new System.Drawing.Point(3, 298);
+            this.buttonAlmyTra.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonAlmyTra.Name = "buttonAlmyTra";
-            this.buttonAlmyTra.Size = new System.Drawing.Size(178, 76);
+            this.buttonAlmyTra.Size = new System.Drawing.Size(178, 165);
             this.buttonAlmyTra.TabIndex = 24;
             this.buttonAlmyTra.Text = "Almacen y trabajadores";
             this.buttonAlmyTra.UseVisualStyleBackColor = true;
@@ -116,9 +109,10 @@
             this.buttonMantFr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonMantFr.Font = new System.Drawing.Font("Segoe UI Black", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonMantFr.ForeColor = System.Drawing.Color.White;
-            this.buttonMantFr.Location = new System.Drawing.Point(3, 249);
+            this.buttonMantFr.Location = new System.Drawing.Point(3, 467);
+            this.buttonMantFr.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonMantFr.Name = "buttonMantFr";
-            this.buttonMantFr.Size = new System.Drawing.Size(178, 76);
+            this.buttonMantFr.Size = new System.Drawing.Size(178, 152);
             this.buttonMantFr.TabIndex = 25;
             this.buttonMantFr.Text = "Mantenimiento";
             this.buttonMantFr.UseVisualStyleBackColor = true;
@@ -137,6 +131,17 @@
             this.flowLayoutPanel1.Size = new System.Drawing.Size(181, 700);
             this.flowLayoutPanel1.TabIndex = 23;
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Location = new System.Drawing.Point(172, 83);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1110, 719);
+            this.panel2.TabIndex = 22;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -146,7 +151,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.panel2);
-            this.ForeColor = System.Drawing.Color.OliveDrab;
+            this.ForeColor = System.Drawing.Color.Olive;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
--- Proyecto_MOANSO_Grupo_05/MaterialesForm.Designer.cs
+++ Proyecto_MOANSO_Grupo_05/MaterialesForm.Designer.cs
@@ -29,21 +29,9 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.dataGriMateriales = new System.Windows.Forms.DataGridView();
-            this.button3 = new System.Windows.Forms.Button();
-            this.txtBuscarMa = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.txtCodigoM = new System.Windows.Forms.TextBox();
             this.txtNombreM = new System.Windows.Forms.TextBox();
-            this.txtDescripcionM = new System.Windows.Forms.TextBox();
             this.txtstockM = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.CBMateriales = new System.Windows.Forms.ComboBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.txtDescripciónMateriales = new System.Windows.Forms.RichTextBox();
             this.label7 = new System.Windows.Forms.Label();
@@ -54,7 +42,6 @@
             this.label13 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.buttonHistorialMat = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGriMateriales)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -67,52 +54,6 @@
             this.button1.Text = "Añadir";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(712, 257);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Modificar";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // dataGriMateriales
-            // 
-            this.dataGriMateriales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGriMateriales.Location = new System.Drawing.Point(31, 445);
-            this.dataGriMateriales.Name = "dataGriMateriales";
-            this.dataGriMateriales.Size = new System.Drawing.Size(426, 175);
-            this.dataGriMateriales.TabIndex = 2;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(854, 240);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Inhabilitar";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // txtBuscarMa
-            // 
-            this.txtBuscarMa.Location = new System.Drawing.Point(782, 306);
-            this.txtBuscarMa.Name = "txtBuscarMa";
-            this.txtBuscarMa.Size = new System.Drawing.Size(100, 20);
-            this.txtBuscarMa.TabIndex = 4;
-            this.txtBuscarMa.TextChanged += new System.EventHandler(this.txtBuscarMa_TextChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(727, 306);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Buscar";
             // 
             // txtCodigoM
             // 
@@ -128,80 +69,12 @@
             this.txtNombreM.Size = new System.Drawing.Size(100, 20);
             this.txtNombreM.TabIndex = 7;
             // 
-            // txtDescripcionM
-            // 
-            this.txtDescripcionM.Location = new System.Drawing.Point(792, 602);
-            this.txtDescripcionM.Name = "txtDescripcionM";
-            this.txtDescripcionM.Size = new System.Drawing.Size(100, 20);
-            this.txtDescripcionM.TabIndex = 8;
-            // 
             // txtstockM
             // 
             this.txtstockM.Location = new System.Drawing.Point(132, 123);
             this.txtstockM.Name = "txtstockM";
             this.txtstockM.Size = new System.Drawing.Size(100, 20);
             this.txtstockM.TabIndex = 9;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(716, 525);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "Codigo";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(712, 568);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 13);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "Nombre";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(712, 607);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 13);
-            this.label4.TabIndex = 12;
-            this.label4.Text = "Descipcion";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(727, 347);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 13;
-            this.label5.Text = "Stock";
-            // 
-            // CBMateriales
-            // 
-            this.CBMateriales.FormattingEnabled = true;
-            this.CBMateriales.Items.AddRange(new object[] {
-            "I",
-            "A"});
-            this.CBMateriales.Location = new System.Drawing.Point(782, 380);
-            this.CBMateriales.Name = "CBMateriales";
-            this.CBMateriales.Size = new System.Drawing.Size(121, 21);
-            this.CBMateriales.TabIndex = 14;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(727, 388);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(40, 13);
-            this.label6.TabIndex = 15;
-            this.label6.Text = "Estado";
             // 
             // groupBox1
             // 
@@ -309,51 +182,25 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.OliveDrab;
-            this.ClientSize = new System.Drawing.Size(984, 693);
+            this.ClientSize = new System.Drawing.Size(610, 422);
             this.Controls.Add(this.buttonHistorialMat);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.CBMateriales);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtDescripcionM);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtBuscarMa);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.dataGriMateriales);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MaterialesForm";
             this.Text = "MaterialesFrom";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGriMateriales)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.DataGridView dataGriMateriales;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox txtBuscarMa;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtCodigoM;
         private System.Windows.Forms.TextBox txtNombreM;
-        private System.Windows.Forms.TextBox txtDescripcionM;
         private System.Windows.Forms.TextBox txtstockM;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ComboBox CBMateriales;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RichTextBox txtDescripciónMateriales;
         private System.Windows.Forms.Label label7;
