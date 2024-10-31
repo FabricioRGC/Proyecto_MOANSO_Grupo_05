efe05ad5026100d54e4825e2f250def78e9fe248
Walter <Walter@LAPTOP-8O0M843J>
2024-10-31 10:29:48 -0500

    Personal avance

--- CapaLogica/logPersonal.cs
+++ CapaLogica/logPersonal.cs
@@ -6,34 +6,37 @@ using System.Threading.Tasks;
 using CapaDatos;
 using static CapaEntidad.Cliente;
 using static CapaEntidad.Personal;
+using CapaEntidad;
 
 namespace CapaLogica
 {
     public class logPersonal
     {
-        //sigleton
+        // Singleton
         private static readonly logPersonal _instancia = new logPersonal();
 
         public static logPersonal Instancia
         {
             get { return logPersonal._instancia; }
         }
+
+        // Método para listar el personal técnico
         public List<entPersonal> listarPersonal()
         {
             return datPersonal.Instancia.listarPersonal();
         }
-        // Metodo para añadir un cliente
-        public void InsertarPersonal(entPersonal cli)
-        {
-            datPersonal.Instancia.InsertarPersonal(cli);
-        }
-        //pruebaa
 
-        // Metodo para deshabilitar un cliente
-        public void DeshabilitarPersonal(entPersonal cli)
+        // Método para añadir un personal
+        public void InsertarPersonal(entPersonal personal)
         {
-            datPersonal.Instancia.DeshabilitaPersonal(cli);
+            datPersonal.Instancia.InsertarPersonal(personal);
+        }
+
+        // Método para deshabilitar un personal
+        public void DeshabilitarPersonal(entPersonal personal)
+        {
+            datPersonal.Instancia.DeshabilitaPersonal(personal);
         }
     }
-
 }
+
