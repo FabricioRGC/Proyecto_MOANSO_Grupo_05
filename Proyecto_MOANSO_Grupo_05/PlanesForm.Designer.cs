65d2489473e1889fcd285275d6100130d62b38d9
Fabri <fabricioronaldogil@gmail.com>
2024-11-16 13:06:42 -0500

    Creacion de botones y finalizacion de interfaz :3

--- Proyecto_MOANSO_Grupo_05/PlanesForm.Designer.cs
+++ Proyecto_MOANSO_Grupo_05/PlanesForm.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.txtcaractplan = new System.Windows.Forms.TextBox();
             this.txttipser = new System.Windows.Forms.TextBox();
@@ -50,16 +49,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewplan)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(396, 374);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 22);
-            this.textBox1.TabIndex = 23;
-            // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.Peru;
             this.groupBox1.Controls.Add(this.txtcaractplan);
             this.groupBox1.Controls.Add(this.txttipser);
             this.groupBox1.Controls.Add(this.txtpreciomen);
@@ -72,11 +64,12 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.groupBox1.Location = new System.Drawing.Point(22, 37);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(323, 374);
+            this.groupBox1.Size = new System.Drawing.Size(323, 329);
             this.groupBox1.TabIndex = 22;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Servicios de internet";
@@ -132,112 +125,133 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Linen;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(23, 164);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(100, 16);
+            this.label4.Size = new System.Drawing.Size(111, 17);
             this.label4.TabIndex = 5;
             this.label4.Text = "Precio Mensual";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Linen;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(20, 210);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(104, 16);
+            this.label5.Size = new System.Drawing.Size(114, 17);
             this.label5.TabIndex = 4;
             this.label5.Text = "Tipo de servicio";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Linen;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(19, 255);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(96, 16);
+            this.label6.Size = new System.Drawing.Size(106, 17);
             this.label6.TabIndex = 3;
             this.label6.Text = "Caracteristicas";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Linen;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(20, 121);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.Size = new System.Drawing.Size(117, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "Limites de datos";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Linen;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(19, 74);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 16);
+            this.label2.Size = new System.Drawing.Size(113, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Velocidad Mbps";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Linen;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(19, 32);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 16);
+            this.label1.Size = new System.Drawing.Size(116, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre del plan";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(353, 129);
+            this.button3.BackColor = System.Drawing.Color.SaddleBrown;
+            this.button3.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(811, 25);
             this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.Size = new System.Drawing.Size(120, 36);
             this.button3.TabIndex = 21;
             this.button3.Text = "Inhabilitar ";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(353, 93);
+            this.button2.BackColor = System.Drawing.Color.SaddleBrown;
+            this.button2.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(659, 25);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.Size = new System.Drawing.Size(120, 36);
             this.button2.TabIndex = 20;
             this.button2.Text = "Modificar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(353, 56);
+            this.button1.BackColor = System.Drawing.Color.SaddleBrown;
+            this.button1.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(483, 25);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(120, 36);
             this.button1.TabIndex = 19;
             this.button1.Text = "Añadir";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dataGridViewplan
             // 
             this.dataGridViewplan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewplan.Location = new System.Drawing.Point(480, 40);
+            this.dataGridViewplan.Location = new System.Drawing.Point(368, 90);
             this.dataGridViewplan.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridViewplan.Name = "dataGridViewplan";
             this.dataGridViewplan.RowHeadersWidth = 51;
-            this.dataGridViewplan.Size = new System.Drawing.Size(568, 300);
+            this.dataGridViewplan.Size = new System.Drawing.Size(679, 276);
             this.dataGridViewplan.TabIndex = 18;
             // 
             // PlanesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1082, 543);
-            this.Controls.Add(this.textBox1);
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(1084, 386);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -249,13 +263,10 @@
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewplan)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox txtcaractplan;
         private System.Windows.Forms.TextBox txttipser;
@@ -269,9 +280,9 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridViewplan;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridViewplan;
     }
 }
--- Proyecto_MOANSO_Grupo_05/PlanesForm.cs
+++ Proyecto_MOANSO_Grupo_05/PlanesForm.cs
@@ -17,24 +17,82 @@ namespace Proyecto_MOANSO_Grupo_05
         public PlanesForm()
         {
             InitializeComponent();
-
+            ListarPlanes();
         }
-
-
+        public void ListarPlanes()
+        {
+            dataGridViewplan.DataSource = logPlanes.Instancia.ListarPlanes();
+        }
+        private void LimpiarVariables()
+        {
+            txtnomplan.Text = "";
+            txtVelocidadmbps.Text = "";
+            txtlimites.Text = "";
+            txtpreciomen.Text = "";
+            txttipser.Text = "";
+            txtcaractplan.Text = "";
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                entPlanes plan = new entPlanes();
+                plan.PlanNombre = txtnomplan.Text.Trim();
+                plan.VelocidadMbps = int.Parse(txtVelocidadmbps.Text);
+                plan.LimiteDeDatos = int.Parse(txtlimites.Text);
+                plan.PrecioMensual = float.Parse(txtpreciomen.Text);
+                plan.TipoServicio = txttipser.Text.Trim();
+                plan.Caracteristicas = txtcaractplan.Text.Trim();
+                plan.estado = "ACTIVO";
 
+                logPlanes.Instancia.InsertarPlanes(plan);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            LimpiarVariables();
+            ListarPlanes();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            try
+            {
+                entPlanes plan = new entPlanes();
+                plan.PlanNombre = txtnomplan.Text.Trim();
+                plan.VelocidadMbps = int.Parse(txtVelocidadmbps.Text);
+                plan.LimiteDeDatos = int.Parse(txtlimites.Text);
+                plan.PrecioMensual = float.Parse(txtpreciomen.Text);
+                plan.TipoServicio = txttipser.Text.Trim();
+                plan.Caracteristicas = txtcaractplan.Text.Trim();
 
+                logPlanes.Instancia.EditarPlanes(plan);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            LimpiarVariables();
+            ListarPlanes();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                entPlanes plan = new entPlanes();
+                plan.PlanNombre = txtnomplan.Text.Trim();
+                logPlanes.Instancia.DeshabilitarPlanes(plan);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            LimpiarVariables();
+            txtnomplan.Text = "";
+            ListarPlanes();
         }
     }
 }
