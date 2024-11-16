e9ebcf266efb96211a4dbb7b9e10cb3720756f70
Fabri <fabricioronaldogil@gmail.com>
2024-11-16 12:55:47 -0500

    Capa logica logPlanes

--- CapaLogica/logPlanes.cs
+++ CapaLogica/logPlanes.cs
@@ -11,6 +11,36 @@ namespace CapaLogica
 {
     public class logPlanes
     {
-        
+        private static readonly logPlanes _instancia = new logPlanes();
+
+        public static logPlanes Instancia
+        {
+            get { return logPlanes._instancia; }
+        }
+
+        // Metodo para listar los materiales
+        public List<entPlanes> ListarPlanes()
+        {
+            return datPlanes.Instancia.ListarPlanes();
+        }
+
+        // Metodo para añadir un Planes
+        public void InsertarPlanes(entPlanes plan)
+        {
+            datPlanes.Instancia.InsertarPlanes(plan);
+        }
+
+
+        // Metodo para actualizar un Planes
+        public void EditarPlanes(entPlanes plan)
+        {
+            datPlanes.Instancia.EditarPlanes(plan);
+        }
+
+        // Metodo para deshabilitar un Planes
+        public void DeshabilitarPlanes(entPlanes plan)
+        {
+            datPlanes.Instancia.DeshabilitarPlanes(plan);
+        }
     }
 }
