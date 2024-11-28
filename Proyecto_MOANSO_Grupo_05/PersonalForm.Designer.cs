1659958fdd348ae8c5b9c152d1c13d37083b241e
Steven <st22vsc@gmail.com>
2024-11-27 23:32:01 -0500

    Termine el boton Personal

--- Proyecto_MOANSO_Grupo_05/PersonalForm.Designer.cs
+++ Proyecto_MOANSO_Grupo_05/PersonalForm.Designer.cs
@@ -285,7 +285,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(28, 106);
+            this.button1.Location = new System.Drawing.Point(19, 106);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 51;
